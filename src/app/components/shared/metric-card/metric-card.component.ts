47885ed192845eac4f625ca45ee77eb1293a0170
Luiz Marinelli <luizh0508@gmail.com>
2024-05-22 15:11:45 -0300

    feat: metric cards

--- src/app/components/shared/metric-card/metric-card.component.ts
+++ src/app/components/shared/metric-card/metric-card.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-metric-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './metric-card.component.html',
-  styleUrl: './metric-card.component.scss'
+  styleUrl: './metric-card.component.scss',
 })
 export class MetricCardComponent {
+  @Input() metric: any;
+  percentage = 0;
 
+  ngOnInit(): void {
+    this.percentage =
+      (this.metric.currentMileage / this.metric.maxMileage) * 100;
+  }
 }
