5f9fdb6f3c15655f1515b5863ada6cd95417e8a2
Luiz Marinelli <luizh0508@gmail.com>
2024-05-22 14:50:26 -0300

    feat: app imports, local storage setters and mock import done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/shared/header/header.component';
+
+import renegade from '../assets/mocks/renegade.json';
+import porsche from '../assets/mocks/911.json';
+import rs6 from '../assets/mocks/rs6.json';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +13,16 @@ import { HeaderComponent } from './components/shared/header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'car-helper-app';
+
+  ngOnInit(): void {
+    window.localStorage.setItem('selectedVehicle', JSON.stringify(renegade));
+    window.localStorage.setItem('allVehicles', this.compileVehicles());
+  }
+
+  compileVehicles(): string {
+    const vehicles = [renegade, porsche, rs6];
+    return JSON.stringify(vehicles);
+  }
 }
