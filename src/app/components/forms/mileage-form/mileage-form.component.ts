373a2bc3b0d729b8e351cdceca0b31e9d541bde8
Luiz Marinelli <luizh0508@gmail.com>
2024-05-22 15:35:28 -0300

    feat: mileage form

--- src/app/components/forms/mileage-form/mileage-form.component.ts
+++ src/app/components/forms/mileage-form/mileage-form.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CarInfoComponent } from '../../shared/car-info/car-info.component';
 
 @Component({
   selector: 'app-mileage-form',
   standalone: true,
-  imports: [],
+  imports: [CarInfoComponent, FormsModule],
   templateUrl: './mileage-form.component.html',
-  styleUrl: './mileage-form.component.scss'
+  styleUrl: './mileage-form.component.scss',
 })
 export class MileageFormComponent {
+  constructor(private router: Router) {}
+  vehicle: any;
+  mileage: any;
+  ngOnInit(): void {
+    this.vehicle = JSON.parse(window.localStorage.getItem('selectedVehicle')!);
+    this.mileage = this.vehicle.mileage;
+  }
 
+  update() {
+    const diff = this.mileage - this.vehicle.mileage;
+    this.vehicle.mileage = this.mileage;
+
+    this.vehicle.metrics.forEach((element: any) => {
+      element.currentMileage += diff;
+    });
+
+    this.updateAllVehicles();
+  }
+
+  updateAllVehicles() {
+    const vehicles = JSON.parse(window.localStorage.getItem('allVehicles')!);
+    const index = vehicles
+      .map((e: any) => e.licensePlate)
+      .indexOf(this.vehicle.licensePlate);
+    vehicles[index] = this.vehicle;
+
+    window.localStorage.removeItem('allVehicles');
+    window.localStorage.setItem('allVehicles', JSON.stringify(vehicles));
+
+    window.localStorage.removeItem('selectedVehicle');
+    window.localStorage.setItem(
+      'selectedVehicle',
+      JSON.stringify(this.vehicle)
+    );
+
+    this.router.navigateByUrl('');
+  }
 }
