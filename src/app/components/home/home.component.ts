ea55acd97e7d8b6727cf1b4dcb6e5dcbd7bfd747
Luiz Marinelli <luizh0508@gmail.com>
2024-05-22 15:30:39 -0300

    feat: home page done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarInfoComponent } from '../shared/car-info/car-info.component';
+import { MetricCardComponent } from '../shared/metric-card/metric-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarInfoComponent, MetricCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  metrics: any[] = [];
 
+  ngOnInit(): void {
+    this.metrics = JSON.parse(
+      window.localStorage.getItem('selectedVehicle')!
+    ).metrics;
+
+    this.metrics = this.metrics.filter((value) => value.liked);
+  }
 }
