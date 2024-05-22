83cbe68453e8334ffa6c2df41b228f37943ae0e3
Luiz Marinelli <luizh0508@gmail.com>
2024-05-22 15:25:13 -0300

    feat: metrics page done

--- src/app/components/metrics/metrics.component.ts
+++ src/app/components/metrics/metrics.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CarInfoComponent } from '../shared/car-info/car-info.component';
+import { MetricCardComponent } from '../shared/metric-card/metric-card.component';
 
 @Component({
   selector: 'app-metrics',
   standalone: true,
-  imports: [],
+  imports: [MetricCardComponent, CommonModule, CarInfoComponent],
   templateUrl: './metrics.component.html',
-  styleUrl: './metrics.component.scss'
+  styleUrl: './metrics.component.scss',
 })
-export class MetricsComponent {
+export class MetricsComponent implements OnInit {
+  metrics: any;
 
+  ngOnInit(): void {
+    this.metrics = JSON.parse(
+      window.localStorage.getItem('selectedVehicle')!
+    ).metrics;
+    this.order();
+  }
+
+  order() {
+    this.metrics = this.metrics.sort(
+      (a: any, b: any) => Number(b.liked) - Number(a.liked)
+    );
+  }
 }
