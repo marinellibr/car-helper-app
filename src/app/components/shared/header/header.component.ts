642beb6d11db4b2899d0d7d0e7a0cf1e9d4e4a8d
Luiz Marinelli <luizh0508@gmail.com>
2024-05-22 14:52:30 -0300

    feat: header done

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuComponent } from '../menu/menu.component';
+import { QuickUpdateComponent } from '../quick-update/quick-update.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MenuComponent, QuickUpdateComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
 
+  menuWatcher = false;
+
+  toggleMenu() {
+    this.menuWatcher = !this.menuWatcher;
+  }
+
+  navigate(endpoint: string) {
+    this.router.navigateByUrl(`/${endpoint}`);
+  }
 }
