997599d5518429c10acfa14c6690ae002a9950d6
Luiz Marinelli <luizh0508@gmail.com>
2024-05-22 15:18:22 -0300

    feat: vehicles list

--- src/app/components/vehicles-list/vehicles-list.component.ts
+++ src/app/components/vehicles-list/vehicles-list.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-vehicles-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './vehicles-list.component.html',
-  styleUrl: './vehicles-list.component.scss'
+  styleUrl: './vehicles-list.component.scss',
 })
 export class VehiclesListComponent {
+  constructor(private router: Router, private location: Location) {}
 
+  vehicles: any;
+  selected: any;
+
+  ngOnInit(): void {
+    this.vehicles = JSON.parse(window.localStorage.getItem('allVehicles')!);
+    this.selected = JSON.parse(window.localStorage.getItem('selectedVehicle')!);
+  }
+
+  updateSelected(index: number) {
+    window.localStorage.removeItem('selectedVehicle');
+    window.localStorage.setItem(
+      'selectedVehicle',
+      JSON.stringify(this.vehicles[index])
+    );
+    this.selected = this.vehicles[index];
+    this.location.back();
+  }
 }
