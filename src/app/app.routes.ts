0cbe28be48196c29fa2e6f8d3441aec4de63eb47
Luiz Marinelli <luizh0508@gmail.com>
2024-05-22 15:04:39 -0300

    feat: update on routes and quick update button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MetricsComponent } from './components/metrics/metrics.component';
+import { VehiclesListComponent } from './components/vehicles-list/vehicles-list.component';
+import { MileageFormComponent } from './components/forms/mileage-form/mileage-form.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'metrics', component: MetricsComponent },
+  { path: 'vehicles', component: VehiclesListComponent },
+  { path: 'update-mileage', component: MileageFormComponent },
   { path: '**', pathMatch: 'full', redirectTo: '' },
 ];
--- src/app/components/shared/quick-update/quick-update.component.ts
+++ src/app/components/shared/quick-update/quick-update.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quick-update',
   standalone: true,
   imports: [],
   templateUrl: './quick-update.component.html',
-  styleUrl: './quick-update.component.scss'
+  styleUrl: './quick-update.component.scss',
 })
 export class QuickUpdateComponent {
+  constructor(private router: Router) {}
 
+  navigate() {
+    this.router.navigateByUrl('update-mileage');
+  }
 }
