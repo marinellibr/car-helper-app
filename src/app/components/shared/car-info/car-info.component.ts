8ff55d84bee2fed0a6d182f0ca1e4cd2a5b11ddf
Luiz Marinelli <luizh0508@gmail.com>
2024-05-22 19:23:31 -0300

    feat: car info done and bugfix on metric card

--- src/app/components/shared/car-info/car-info.component.ts
+++ src/app/components/shared/car-info/car-info.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-car-info',
   standalone: true,
   imports: [],
   templateUrl: './car-info.component.html',
-  styleUrl: './car-info.component.scss'
+  styleUrl: './car-info.component.scss',
 })
-export class CarInfoComponent {
+export class CarInfoComponent implements OnInit {
+  constructor(private router: Router) {}
+  selected: any;
 
+  ngOnInit(): void {
+    this.selected = JSON.parse(window.localStorage.getItem('selectedVehicle')!);
+  }
+
+  navigate(endpoint: string) {
+    this.router.navigateByUrl(`/${endpoint}`);
+  }
 }
