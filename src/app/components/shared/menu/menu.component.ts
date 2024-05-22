8a4571d826413832deee4cd32257506e8b93b487
Luiz Marinelli <luizh0508@gmail.com>
2024-05-22 14:54:56 -0300

    feat: reseting padding and margin globally, menu done

--- src/app/components/shared/menu/menu.component.ts
+++ src/app/components/shared/menu/menu.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
+  constructor(private route: Router) {}
 
+  @Output() newMenuStatus = new EventEmitter<boolean>();
+
+  navigate(endpoint: string) {
+    this.newMenuStatus.emit(false);
+    this.route.navigateByUrl(`/${endpoint}`);
+  }
 }
